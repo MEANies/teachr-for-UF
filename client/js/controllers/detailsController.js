95d5618f8b1cc6c27f5c7a88523e6110843eb10b
Liam Wood <liamvwood@ufl.edu>
2018-12-05 10:15:44 -0500

    setting up student

--- client/js/controllers/detailsController.js
+++ client/js/controllers/detailsController.js
@@ -1,12 +1,3 @@
-angular.module('directoryApp').controller('DetailsInstanceController', function ($rootScope, $uibInstanceModal, User, Auth, Courses) {
-    var $ctrl = this;
-        
-    $ctrl.ok = function () {
-        $uibModalInstance.close('Closed');
-      };
-    
-      $ctrl.cancel = function () {
-        console.log("Debug: Edit Modal Canceled");
-        $uibModalInstance.dismiss('home');
-      };
+angular.module('directoryApp').controller('DetailsInstanceController', function ($rootScope, User, Auth, Courses) {
+
 })
