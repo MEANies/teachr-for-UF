bd54d787e425cff5c9837bf552b8608e67ecf522
Liam Wood <liamvwood@ufl.edu>
2018-12-05 12:05:15 -0500

    hardcoding

--- client/js/controllers/coursesController.js
+++ client/js/controllers/coursesController.js
@@ -10,6 +10,18 @@ angular.module('listings').controller('CoursesController', ['$scope', 'Courses',
     Listings.getAll().then(function(response) {
       console.log("debug");
       $scope.courses = response.data.courses;
+      console.log($scope.courses);
+      $scope.courses.push(
+        {
+          code: 'CEN3031',
+          name: 'Introduction to Software Engineering',
+          department: 'Computer & Information Science & Engineering',
+          instructor_names: ['Philippa Brown'],
+          description: 'Topics include software planning, specifications, coding, testing and maintenance. Gain experience in the team approach to large system development. (M)',
+          building: '',
+          building_code: ''
+        }
+      )
       $scope.admins = response.data.admins;
       console.log(response.data);
     }, function(error) {
